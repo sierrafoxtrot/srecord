181aac84ed818f33b9974fd8cfb1fe67c86c9d04
Peter Miller <pmiller@opensource.org.au>
2000-12-06 21:13:07 +0000

    build glitch
    This change adds an include of <stddef.h> to include/srec/memory/chunk.h because it cauased a build problem on some platforms.
    
    My thanks to Hendrik De Vloed <hendrik.devloed@barco.com> for reporting this problem.

--- include/srec/memory/chunk.h
+++ include/srec/memory/chunk.h
@@ -1,6 +1,6 @@
 /*
  *	srecord - manipulate eprom load files
- *	Copyright (C) 1998, 1999 Peter Miller;
+ *	Copyright (C) 1998, 1999, 2000 Peter Miller;
  *	All rights reserved.
  *
  *	This program is free software; you can redistribute it and/or modify
@@ -24,6 +24,8 @@
 #define INCLUDE_SREC_MEMORY_CHUNK_H
 
 #pragma interface "srec_memory_chunk"
+
+#include <stddef.h>
 
 class srec_memory_chunk
 {
