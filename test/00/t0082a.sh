3e09ae60ea7b30184e36546a360ee3577cb066b5
Peter Miller <pmiller@opensource.org.au>
2006-12-20 23:25:38 +0000

    copyrights The copyright norices have been changed to remove the "all rights reserved" phrase.  It did not represent the GPL spirit.

--- test/00/t0082a.sh
+++ test/00/t0082a.sh
@@ -1,8 +1,7 @@
 #!/bin/sh
 #
 #	srecord - manipulate eprom load files
-#	Copyright (C) 2003, 2006 Peter Miller;
-#	All rights reserved.
+#	Copyright (C) 2003, 2006 Peter Miller
 #
 #	This program is free software; you can redistribute it and/or modify
 #	it under the terms of the GNU General Public License as published by
