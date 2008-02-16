ad99da393a645127df52ed062b7d96bb2132faee
Peter Miller <pmiller@opensource.org.au>
2008-02-16 05:02:25 +0000

    gcc 4.3 support This change set adds support for GCC 4.3 to the code base.
    My thanks to Fathi Boudra <fabo@debian.org> for this patch.  Fathi is the Debian maintainer for SRecord.

--- lib/arglex.cc
+++ lib/arglex.cc
@@ -1,6 +1,6 @@
 //
 //      srecord - manipulate eprom load files
-//      Copyright (C) 1998, 1999, 2002, 2003, 2006, 2007 Peter Miller
+//      Copyright (C) 1998, 1999, 2002, 2003, 2006-2008 Peter Miller
 //
 //      This program is free software; you can redistribute it and/or modify
 //      it under the terms of the GNU General Public License as published by
@@ -20,6 +20,7 @@
 
 #include <cctype>
 #include <cstring>
+#include <cstdlib>
 #include <errno.h>
 #include <iostream>
 using namespace std;
--- lib/quit/normal.cc
+++ lib/quit/normal.cc
@@ -1,6 +1,6 @@
 //
 //      srecord - manipulate eprom load files
-//      Copyright (C) 2000, 2002, 2006, 2007 Peter Miller
+//      Copyright (C) 2000, 2002, 2006-2008 Peter Miller
 //
 //      This program is free software; you can redistribute it and/or modify
 //      it under the terms of the GNU General Public License as published by
@@ -18,6 +18,8 @@
 //
 
 #include <cstdarg>
+#include <cstdlib>
+#include <cstring>
 #include <iostream>
 
 #include <lib/progname.h>
