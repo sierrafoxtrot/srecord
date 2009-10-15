ef25b341784b158d9a96375f5f1b5f0be514b05d
Peter Miller <pmiller@opensource.org.au>
2009-10-15 12:25:19 +0000

    lintian warnings The warning about hyphen in the manual pages has been silenced, by careful use of \-, \[hy\ and \[mi] as appropriate.   Sure makes some of the sources ugly, tho.
    The warning about the undefined .XX macro has been silenced, by making it conditional.
    
    I am not completely happy with the fundamentalist pedantry preached by some Debian packagers.  These are only warnings, after all.

--- lib/srec/output/file/forth.cc
+++ lib/srec/output/file/forth.cc
@@ -1,7 +1,7 @@
 //
 // srecord - manipulate eprom load files
-// Copyright (C) 2008 Peter Miller
-// Copyright (C) 2008 Torsten Sadowsk
+// Copyright (C) 2008, 2009 Peter Miller
+// Copyright (C) 2008 Torsten Sadowski
 //
 // This program is free software; you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
