601c9c2270a9f693ce5bdb0b10128bafcd2e6a54
Peter Miller <pmiller@opensource.org.au>
2010-01-27 20:29:08 +0000

    shared library, part 16, LGPL The license on the shared library code has been updated to be LGPLv3+ to permit use under the "system library" provisions.

--- srecord/output/file/msbin.h
+++ srecord/output/file/msbin.h
@@ -1,10 +1,12 @@
 //
 // srecord - manipulate eprom load files
-// Copyright (C) 2009 David Kozub <zub@linux.fjfi.cvut.cz>
 // Copyright (C) 2009, 2010 Peter Miller
 //
-// This program is free software; you can redistribute it and/or modify
-// it under the terms of the GNU General Public License as published by
+// Code contribution by David Kozub <zub@linux.fjfi.cvut.cz>
+// Copyright assigned to Peter Miller 27-Jan-2010.
+//
+// This program is free software; you can redistribute it and/or modify it
+// under the terms of the GNU Lesser General Public License as published by
 // the Free Software Foundation; either version 3 of the License, or (at
 // your option) any later version.
 //
@@ -13,7 +15,7 @@
 // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
 // General Public License for more details.
 //
-// You should have received a copy of the GNU General Public License
+// You should have received a copy of the GNU Lesser General Public License
 // along with this program. If not, see <http://www.gnu.org/licenses/>.
 //
 
