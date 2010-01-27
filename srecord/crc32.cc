5ffdfdede5aac8008dc723bdc6304cfd2ceb23a8
Peter Miller <pmiller@opensource.org.au>
2010-01-27 04:20:07 +0000

    shared library, part 14, LGPL The license on the shared library code has been updated to be LGPLv3+ to permit use under the "system library" provisions.

--- srecord/crc32.cc
+++ srecord/crc32.cc
@@ -1,23 +1,23 @@
 //
-//      srecord - manipulate eprom load files
-//      Copyright (C) 2000-2002, 2006-2010 Peter Miller
+// srecord - manipulate eprom load files
+// Copyright (C) 2000-2002, 2006-2010 Peter Miller
 //
-//      This program is free software; you can redistribute it and/or modify
-//      it under the terms of the GNU General Public License as published by
-//      the Free Software Foundation; either version 3 of the License, or
-//      (at your option) any later version.
+// This program is free software; you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation; either version 3 of the License, or
+// (at your option) any later version.
 //
-//      This program is distributed in the hope that it will be useful,
-//      but WITHOUT ANY WARRANTY; without even the implied warranty of
-//      MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-//      GNU General Public License for more details.
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Lesser General Public License for more details.
 //
-//      You should have received a copy of the GNU General Public License
-//      along with this program. If not, see
-//      <http://www.gnu.org/licenses/>.
+// You should have received a copy of the GNU Lesser General Public License
+// along with this program. If not, see
+// <http://www.gnu.org/licenses/>.
 //
-//      (I got this copy from http://www.gator.net/~garyg/C/CONTRIB/SNIP/
-//      but as always on the Internet, it is now gone.  So use the
+// (I got this copy from http://www.gator.net/~garyg/C/CONTRIB/SNIP/
+// but as always on the Internet, it is now gone.  So use the
 //      Wayback Machine, http://www.archive.org/, or google for "crc_32"
 //      and you should find it fairly quickly.  The same code also
 //      appears in the ZModem sources, as well, but they seem to abuse
